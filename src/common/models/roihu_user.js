01295ab18b853a108e37e71354433d1833316e3d
Emil Virkki <emil.virkki@gmail.com>
2016-03-06 21:08:58 +0200

    Implement login with PartioID

---
+++ src/common/models/roihu_user.js
@@ -0,0 +1,12 @@
+export default function(RoihuUser) {
+
+  RoihuUser.findByMemberNumber = function(memberNumber) {
+    const query = {
+      where: {
+        memberNumber: memberNumber,
+      },
+    };
+    return RoihuUser.findOne(query);
+  };
+
+}
