8877c334bb96d473b16318771261fc6d88ec7d7e
Matias Turunen <mt95@windowslive.com>
2016-03-11 17:42:27 +0200

    Added automatic translation import

---
+++ scripts/create-translations.js
@@ -0,0 +1,72 @@
+import path from 'path';
+import Promise from 'bluebird';
+import app from '../src/server/server.js';
+import _ from 'lodash';
+import uuid from 'uuid';
+
+const TranslationModel = app.models.Translation;
+const createTranslation = Promise.promisify(TranslationModel.create, {context: TranslationModel});
+
+const translateableList = [
+  'Instruction',
+];
+
+function getTranslations(modelName) {
+	return Promise.try(() => require(path.resolve(__dirname, `./translations_to_create/${modelName}`)));
+}
+
+function createTranslationsForModel(modelName) {
+	const model = app.models[modelName];
+	const createModel = Promise.promisify(model.create, {context: model});
+
+	return getTranslations(modelName)
+		.then(modelData => {
+				_.forEach(modelData, function(fixture) {
+					console.log(fixture);
+					var modelJSON = {};
+					var translations = [];
+
+					_.forEach(fixture, function(value, key) {
+						if(typeof value === 'object') {
+							// generate model JSON with uuids in case of translation
+							var textUuid = uuid.v1();
+							modelJSON[key] = textUuid;
+
+							// add translations too
+							_.forEach(value, function(text, lang) {
+								translations.push({
+									"lang": lang,
+									"text": text,
+									"guid": textUuid
+								});
+							});
+						} else {
+							modelJSON[key] = value;
+						}
+						
+					});
+					console.log(modelJSON);
+					createModel(modelJSON)
+						.then(createTranslation(translations))
+					  .catch(err => console.error(`Failed to create translations for model $(modelName): `, err));
+				});
+
+		});
+}
+
+// Pikkukikka joka suorittaa promiseReturningFunctionin peräkkäin jokaiselle values-listan jäsenelle niin,
+// että promiseReturningFunctioneja on vain yksi suorituksessa kerrallaan.
+// Palauttaa tyhjän resolved-tilassa olevan promisen jos values-lista on tyhjä.
+// kopioitu leirireksteristä
+function forAll(values, promiseReturningFunction) {
+  return values.reduce((cur, next) => cur.then(() => promiseReturningFunction(next)), Promise.resolve());
+}
+
+export function createTranslations() {
+	return forAll(translateableList, createTranslationsForModel);
+}
+
+if (require.main === module) {
+	createTranslations()
+		.catch(err => console.error('Failed to create translations: ', err));
+}
