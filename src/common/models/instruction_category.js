912c77e6c4c84c4d86a3044faa7634276dba4a33
Matias Turunen <mt95@windowslive.com>
2016-03-19 17:00:47 +0200

    Instruction category will return translated instructions as well as categories

--- src/common/models/instruction_category.js
+++ src/common/models/instruction_category.js
@@ -1,21 +1,69 @@
-//import app from '../../server/server';
+import app from '../../server/server';
+import Promise from 'bluebird';
 import * as translationUtils from '../utils/translations';
+import _ from 'lodash';
 
 module.exports = function(InstructionCategory) {
 
-  InstructionCategory.FindInstrunctions = function(lang, cb) {
+  InstructionCategory.FindTranslations = function(lang, cb) {
+    const Instruction = app.models.Instruction;
 
+    if (!lang || !translationUtils.langExists(lang)) {
+      lang = 'EN';
+    }
+
+    const timeNow = Date.now();
+    const response = {
+      'timestamp': timeNow,
+      'language': lang,
+    };
+    const rCategories = [];
     translationUtils.getTranslationsForModel(InstructionCategory, lang)
-      .then(tr => cb(null, tr));
+      .then(categoryTranslations => {
+        const promises = [];
+        _.forEach(categoryTranslations, category => {
+          const catArticles = [];
+          const instructionPromise = translationUtils.getTranslationsForModel(Instruction, lang, { where: { categoryId: category.idFromSource } })
+            .then(instructionTranslations => {
+              const catInstr = [];
+              _.forEach(instructionTranslations, instruct => {
+                catInstr.push({             // add single instruction
+                  'title': instruct.name,
+                  'bodytext': instruct.description,
+                  'sort_no': instruct.sortNo,
+                  'last_modified': instruct.lastModified,
+                  'id': instruct.id,
+                });
+              });
+              catArticles.push(catInstr);   // add instruction group to category
+
+            })
+            .then(rCategories.push({
+              'title': category.name,
+              'id': category.id,
+              'sort_no': category.sortNo,
+              'last_modified': category.lastModified,
+              'articles': catArticles,
+            }));
+
+          promises.push(instructionPromise);
+        });
+
+        Promise.all(promises)
+          .then(() => {
+            response.categories = rCategories;
+            cb(null, response);
+          });
+      });
 
   };
 
   InstructionCategory.remoteMethod(
-    'FindInstrunctions',
+    'FindTranslations',
     {
-      http: { path: '/instructions', verb: 'get' },
+      http: { path: '/translations', verb: 'get' },
       accepts: [
-        { arg: 'lang', type: 'string', http: { source: 'query' }, required: true },
+        { arg: 'lang', type: 'string', http: { source: 'query' }, required: false },
       ],
       returns: { type: 'array', root: true },
     }
