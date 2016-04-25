08cf2179f64f74cd9d77ba3f79970eb1586cbfbb
Matias Turunen <mt95@windowslive.com>
2016-04-25 13:30:52 +0300

    Remove old models when updating data

--- scripts/load-sharepoint.js
+++ scripts/load-sharepoint.js
@@ -8,78 +8,63 @@ export function loadLocations() {
   const Location = app.models.Location;
   const findOrCreateLocationCategory = Promise.promisify(LocationCategory.findOrCreate, { context: LocationCategory });
   const findOrCreateLocation = Promise.promisify(Location.findOrCreate, { context: Location });
-  const sharepoint = require('sharepointconnector')({
-    username : process.env.SHAREPOINT_USER,
-    password : process.env.SHAREPOINT_PSW,
-    // Authentication type - current valid values: ntlm, basic, online,onlinesaml 
-    type : 'online',
-    url : 'https://partio.sharepoint.com',
-    context : 'roihu/tyokalut',
-  });
-  sharepoint.login(function(err){
-    if (err){
-      return console.error(err);
-    }
-    const categories = [];
-    const catNames = [];
-    const locations = [];
 
-    let categoryIndex = 1;
-    let categorySortNo = 1;
-    let locIndex = 1;
-    let locSortNo = 1;
+  const categories = [];
+  const catNames = [];
+  const locations = [];
 
-    // Load 'Paikat' list from sharepoint
-    sharepoint.listItems.list('Paikat', (err, data) => {
-      if (err) console.error(err);
-      _.forEach(data, item => {
-        const categoryObj = {
-          'name': {
-            'FI': item.Kategoria,
-          },
-          'sortNo': categorySortNo,
-          'idFromSource': categoryIndex,
-        };
-        // compare categories with their name instead of full object
-        let cIndexNum = catNames.indexOf(item.Kategoria);
-        if ( cIndexNum === -1) {
-          categories.push(categoryObj);
-          catNames.push(item.Kategoria);
-          cIndexNum = categoryIndex-1;
-          categoryIndex += 1;
-          categorySortNo += 1;
-        }
-        locations.push({
-          'name': {
-            'FI': item.Title,
-          },
-          'description': {
-            'FI': item.Kuvaus || '',
-          },
-          'sortNo': locSortNo++,
-          'idFromSource': locIndex++,
-          'categoryId': cIndexNum + 1,
-          'gpsLatitude': item.Latitude,
-          'gpsLongitude': item.Longitude,
-          'gridLatitude': item.Koordinaattiruutu.substring(0,1),
-          'gridLongitude': item.Koordinaattiruutu.substring(1,3),
-        });
-      });
-/*
-      console.log('G_----------------------------');
-      console.log(categories);
-      console.log('LOC_----------------------------');
-      console.log(locations);
-*/
-      translationUtils.createTranslationsForModel('LocationCategory', categories).then(cr => {
-        console.log(cr);
-      });
-      translationUtils.createTranslationsForModel('Location', locations).then(loc => {
-        console.log(loc);
-      });
+  let categoryIndex = 1;
+  let categorySortNo = 1;
 
+  readSharepointList('Paikat', function(data) {
+    _.forEach(data, item => {
+      const categoryObj = {
+        'name': {
+          'FI': item.Kategoria,
+        },
+        'sortNo': categorySortNo,
+        'idFromSource': categoryIndex,
+      };
+      // compare categories with their name instead of full object
+      let cIndexNum = catNames.indexOf(item.Kategoria);
+      if ( cIndexNum === -1) {
+        categories.push(categoryObj);
+        catNames.push(item.Kategoria);
+        cIndexNum = categoryIndex-1;
+        categoryIndex += 1;
+        categorySortNo += 1;
+      }
+      locations.push({
+        'name': {
+          'FI': item.Title,
+          'SV': selfOrEmpty(item.Otsikko_x0020_ruotsiksi),
+          'EN': selfOrEmpty(item.Otsikko_x0020_englanniksi),
+        },
+        'description': {
+          'FI': selfOrEmpty(item.Kuvaus),
+          'SV': selfOrEmpty(item.Kuvaus_x0020_ruotsiksi),
+          'EN': selfOrEmpty(item.Kuvaus_x0020_englanniksi),
+        },
+        'sortNo': item.Id,
+        'idFromSource': item.Id,
+        'categoryId': cIndexNum + 1,
+        'gpsLatitude': selfOrEmpty(item.Latitude),
+        'gpsLongitude': selfOrEmpty(item.Longitude),
+        'gridLatitude': selfOrEmpty(item.Koordinaattiruutu.substring(0,1)),
+        'gridLongitude': selfOrEmpty(item.Koordinaattiruutu.substring(1,3)),
+      });
+    });
+
+    translationUtils.createTranslationsForModel('LocationCategory', categories).then(cr => {
+      // delete all other categories
+      destroyAllByNameGuid('LocationCategory', cr);
+    });
+    translationUtils.createTranslationsForModel('Location', locations).then(loc => {
+      // delete all other locations
+      destroyAllByNameGuid('Location', loc);
     });
   });
+
 }
 
 function isInArray(obj, arr) {
@@ -88,6 +73,47 @@ function isInArray(obj, arr) {
     if(arr[i] == obj) return true;
   }
   return false;
+}
+
+function destroyAllByNameGuid(modelName, guIdList) {
+  const guidsToDelete = [];
+  _.forEach(guIdList, obj => {
+    _.forEach(obj, item => {
+      guidsToDelete.push(item.guId);
+    });
+  });
+
+  // deletes all instances where name guId is not in our array
+  app.models[modelName].destroyAll({ name: { nin: guidsToDelete } }, (err, info) => {
+    if (err) console.error(err);
+    console.log('Deleted', info.count, 'unused instances of', modelName);
+  });
+}
+
+function selfOrEmpty(val) {
+  return val || '';
+}
+
+function readSharepointList(listName, handler) {
+  const sharepoint = require('sharepointconnector')({
+    username : process.env.SHAREPOINT_USER,
+    password : process.env.SHAREPOINT_PSW,
+    // Authentication type - current valid values: ntlm, basic, online,onlinesaml 
+    type : 'online',
+    url : 'https://partio.sharepoint.com',
+    context : 'roihu/tyokalut',
+  });
+
+  sharepoint.login(function(err){
+    if (err) {
+      return console.error(err);
+    }
+
+    sharepoint.listItems.list(listName, (err, data) => {
+      handler(data);
+    });
+
+  });
 }
 
 if (require.main === module) {
