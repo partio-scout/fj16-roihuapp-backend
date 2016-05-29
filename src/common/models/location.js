c9517376cbb30f234adcb511774f4a5e2cca14f2
Matias Turunen <mt95@windowslive.com>
2016-05-29 13:55:31 +0300

    Add grid coordinates validation before save

--- src/common/models/location.js
+++ src/common/models/location.js
@@ -1,20 +1,34 @@
 module.exports = function(Location) {
   Location.observe('before save', (ctx, next) => {
     if (ctx.instance) {
-      if (ctx.instance.gridLatitude && !/^[A-Z]{1}$/.test(ctx.instance.gridLatitude)) {
+      const validateLat = validateGridLatitude(ctx.instance.gridLatitude);
+      const validateLon = validateGridLongitude(ctx.instance.gridLongitude);
+      if (validateLat && validateLon) {
+        ctx.instance.gridLatitude = validateLat;
+        ctx.instance.gridLongitude = validateLon;
+      } else {
         ctx.instance.gridLatitude = '';
-      }
-      if (ctx.instance.gridLongitude && !/^[0-9]{2}$/.test(ctx.instance.gridLongitude)) {
         ctx.instance.gridLongitude = '';
       }
     } else {
-      if (ctx.gridLatitude && !/^[A-Z]{1}$/.test(ctx.gridLatitude)) {
+      const validateLat = validateGridLatitude(ctx.gridLatitude);
+      const validateLon = validateGridLongitude(ctx.gridLongitude);
+      if (validateLat && validateLon) {
+        ctx.gridLatitude = validateLat;
+        ctx.gridLongitude = validateLon;
+      } else {
         ctx.gridLatitude = '';
-      }
-      if (ctx.gridLongitude && !/^[0-9]{2}$/.test(ctx.gridLongitude)) {
         ctx.gridLongitude = '';
       }
     }
     next();
   });
+
+  function validateGridLatitude(lat) {
+    return (/^[A-Za-z]{1}$/.test(lat)) ? lat.toUpperCase() : '';
+  }
+
+  function validateGridLongitude(lon) {
+    return (/^[0-9]{2}$/.test(lon)) ? lon.toUpperCase() : '';
+  }
 };
