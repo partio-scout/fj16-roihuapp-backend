4f873a6c68fd92ded7365c1363a733df24efe1cf
Matias Turunen <mt95@windowslive.com>
2016-06-16 14:48:36 +0300

    Load instruction articles from FJ16 website

--- scripts/load-instructions.js
+++ scripts/load-instructions.js
@@ -1,19 +1,99 @@
 import app from '../src/server/server';
 import Promise from 'bluebird';
 import _ from 'lodash';
-import * as translationUtils from '../src/common/utils/translations';
+import request from 'superagent';
 
-export function instructionsHandler(jsonData, cb) {
-  // cb is for tests
+export function instructionsHandler(articles, categories) {
+  const createInstuction = Promise.promisify(app.models.Instruction.create, { context: app.models.Instruction });
+  const createInstuctionCategory = Promise.promisify(app.models.InstructionCategory.create, { context: app.models.InstructionCategory });
+  return new Promise((resolve, reject) => {
+    console.log('----- ARTICLES --------');
+    console.log(articles);
+    console.log('----- CATEGORIES ------');
+    console.log(categories);
 
-  console.log(jsonData);
+    const instructions = [];
+    const instructionCategories = [];
 
+    _.forEach(articles, article => {
+      instructions.push({
+        instructionId: parseInt(article.nid),
+        categoryId: parseInt(article.field_category_in_participant_in.tid),
+        sortNo: parseInt(article['Sort number'].value),
+        lastModified: article['Päivitetty'],
+        name: article.title,
+        description: article.body.value,
+        lang: getLangCode(article.language),
+      });
+    });
+
+    _.forEach(categories, category => {
+      instructionCategories.push({
+        categoryId: parseInt(category.tid),
+        name: category.name,
+        sortNo: parseInt(category.Paino),
+        lang: getLangCode(category.field_language),
+        lastModified: new Date(),
+      });
+    });
+
+    console.log('-----------------');
+    console.log(instructions);
+    console.log('-----------------');
+    console.log(instructionCategories);
+
+    Promise.join(
+      createInstuctionCategory(instructionCategories),
+      createInstuction(instructions),
+      (inst, cat) => {
+        resolve();
+      }).catch(err => {
+        console.log(err);
+        reject(err);
+      });
+
+  });
+
+  function getLangCode(langText) {
+    const langs = {
+      suomi: 'FI',
+      Finnish: 'FI',
+      ruotsi: 'SV',
+      Swedish: 'SV',
+      englanti: 'EN',
+      English: 'EN',
+    };
+    console.log('LANGTEXT',langText);
+    return langs[langText] || '';
+  }
 }
 
 export function loadInstructions(handler) {
-
+  const baseurl = 'http://fj16-qa.jelastic.planeetta.net/api/infoarticles';
+  return new Promise((resolve, reject) => {
+    request.get(`${baseurl}/participant`)
+    .end((err, articles) => {
+      if (err) reject(err);
+      else {
+        request.get(`${baseurl}/categories`)
+        .end((err, categories) => {
+          if (err) reject(err);
+          else {
+            return handler(articles.body, categories.body);
+          }
+        });
+      }
+    });
+  });
 }
 
 if (require.main === module) {
-  loadInstructions(instructionsHandler);
+  loadInstructions(instructionsHandler)
+  .then(() => {
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
 }
--- src/common/models/instruction_category.js
+++ src/common/models/instruction_category.js
@@ -1,85 +1,50 @@
-import app from '../../server/server';
 import Promise from 'bluebird';
-import * as translationUtils from '../utils/translations';
-import _ from 'lodash';
-import * as errorUtils from '../utils/errors';
 
 module.exports = function(InstructionCategory) {
 
   InstructionCategory.FindTranslations = function(language, afterDate, cb) {
-    const Instruction = app.models.Instruction;
+    const findCategory = Promise.promisify(InstructionCategory.find, { context: InstructionCategory });
 
-    translationUtils.getLangIfNotExists(language)
-      .then(lang => {
+    const timeNow = new Date();
+    const timeNext = new Date(timeNow);
+    timeNext.setHours(timeNow.getHours() + 1);
+    const response = {
+      timestamp: timeNow.toISOString(),
+      next_check: timeNext.toISOString(),
+      language: language,
+      articles: [],
+    };
 
-        const timeNow = new Date();
-        const timeNext = new Date(timeNow);
-        timeNext.setHours(timeNow.getHours() + 1);
-        const response = {
-          'timestamp': timeNow.toISOString(),
-          'next_check': timeNext.toISOString(),
-          'language': lang,
-        };
-        const rCategories = [];
+    let andFilter = [
+      { lang: language },
+    ];
+    if (afterDate) {
+      // Five minustes "safezone" for filtering
+      const afterDate_5min_before = new Date(afterDate);
+      afterDate_5min_before.setMinutes(afterDate_5min_before.getMinutes() - 5);
 
-        translationUtils.getTranslationsForModel(InstructionCategory, lang)
-          .then(categoryTranslations => {
-            const promises = [];
-            _.forEach(categoryTranslations, category => {
-              const catArticles = [];
-              let articleFilter = { where: { categoryId: category.idFromSource } };
+      andFilter = [
+        { lang: language },
+        { lastModified: afterDate_5min_before },
+      ];
+    }
 
-              if (afterDate) {
-                // Five minustes "safezone" for filtering
-                const afterDate_5min_before = new Date(afterDate);
-                afterDate_5min_before.setMinutes(afterDate_5min_before.getMinutes() - 5);
-
-                articleFilter = { where: {
-                  and: [
-                    /*{ lastModified: { gt: afterDate } },*/
-                    { lastModified: { gt: afterDate_5min_before } },
-                    { categoryId: category.idFromSource },
-                  ],
-                } };
-              }
-
-              const instructionPromise = translationUtils.getTranslationsForModel(Instruction, lang, articleFilter)
-                .then(instructionTranslations => {
-                  const catInstr = [];
-                  _.forEach(instructionTranslations, instruct => {
-                    catInstr.push({             // add single instruction
-                      'title': instruct.name,
-                      'bodytext': instruct.description,
-                      'sort_no': instruct.sortNo,
-                      'last_modified': instruct.lastModified,
-                      'id': instruct.instructionId,
-                    });
-                  });
-                  catArticles.push(catInstr);   // add instruction group to category
-
-                })
-                .then(rCategories.push({
-                  'title': category.name,
-                  'id': category.categoryId,
-                  'sort_no': category.sortNo,
-                  'last_modified': category.lastModified,
-                  'articles': catArticles,
-                }))
-                .catch(err => {
-                  cb(errorUtils.createHTTPError('Something went wrong', 500, err.message), null);
-                  return;
-                });
-
-              promises.push(instructionPromise);
-            });
-
-            Promise.all(promises)
-              .then(() => {
-                response.categories = rCategories;
-                cb(null, response);
-              });
-          });
-      });
+    findCategory({
+      where: {
+        lang: language,
+      },
+      include: {
+        relation: 'instructions',
+        scope: {
+          where: {
+            and: andFilter,
+          },
+        },
+      },
+    }).then(categories => {
+      response.articles.push(categories);
+      cb(null, response);
+    });
 
   };
 
